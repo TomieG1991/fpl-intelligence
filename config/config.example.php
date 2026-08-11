44806fff859264b0381014ab8744dbaa3ac304e1
TomieG1991 <tomie_g@hotmail.co.uk>
2026-08-11 11:10:22 +0100

    Add example configuration and secure local config
    Added a configuration example for project setup and excluded the local configuration file from version control.

---
+++ config/config.example.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'database' => [
+        'host' => 'localhost',
+        'name' => 'fpl_intelligence',
+        'username' => 'your_database_username',
+        'password' => 'your_database_password'
+    ],
+    
+    'fpl_api' => [
+        'base_url' => 'https://fantasy.premierleague.com/api/'
+    ]
+
+];
